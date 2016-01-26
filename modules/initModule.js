1c279f1cbead8c55d7847fc928f5046ae5e81a96
Kevin Kim <kevintaekim80@gmail.com>
2016-01-26 10:22:26 -0800

    create initialization module

---
+++ modules/initModule.js
@@ -0,0 +1,79 @@
+//Ektron Module : reponsibile for initialization of folders, ucFiles, etc
+//Dependencies:  helper module, template module
+
+var helper = require('./helper.js');
+var templatesModule = require('./templatesModule.js');
+
+
+//public initlaization methods that will be exported
+
+var initMethods = {
+  createInitDirectories : createInitDirectories,
+  createUCfiles : createUCfiles,
+  createDescriptionFile : createDescriptionFile,
+};
+
+
+function createInitDirectories() {
+  var projectFolderPath = helper.getProjectDir();
+  var initDirectories = helper.getInitialDirectories();
+  //create the base project folder where all other directories will be created in.
+  //create base project folder synchronously to ensure folder exist before other directories are created
+  fs.mkdirSync(projectFolderPath);
+
+  //loop through initDirectories and create folder for each one of them inside the base project folder
+  ///directories can be created async
+  initDirectories.forEach(function(dirName) {
+    var dirPath = projectFolderPath + '/' + dirName;
+    fs.mkdirSync(dirPath);
+  });
+
+  //create UC folder inside 'Layouts directory'
+  fs.mkdirSync(projectFolderPath+ '/Layouts/UC');
+}
+
+//method function to create UC .aspx files for each different layout file inside the Layouts folder
+function createUCfiles() {
+  var projectName = helper.getProjectName();
+  var projectFolderPath = helper.getProjectDir();
+  //the UC folder will be inside the Layouts folder.
+  var basePath = helper.getPathOf('Layouts') + '/' + 'UC';
+
+  var layoutFiles = helper.getFilesOf('Layouts');
+
+  //loop through all the layout files and create UC files for each inside the base
+  /*1. compile complete HTML for each UC file using the render module.
+    Render method must pass object {projectName, fileName}*/
+  layoutFiles.forEach(function(file) {
+    var filePath = basePath + '/' + file['name'] + '.aspx';
+    var bodyObj = {
+      fileName: file['name'],
+      projectName: projectName,
+    };
+    var compiledSrc = renderModule.render['ucFile'](bodyObj);
+
+    //async writeFile to filePath with the compiled src.
+    fs.writeFile(filePath, compiledSrc);
+  });
+
+}
+
+//function method to create description.txt file
+function createDescriptionFile() {
+  //description file will always reside inside the base project directory path
+  var baseDir = helper.getProjectDir();
+  var filePath = baseDir + '/' + 'description.txt';
+
+  //description file template requires {companyName : companyName, projectDate: projectDate} to be passed.
+  var templateObj = {
+    companyName : helper.getCompanyName(),
+    projectDate : helper.getProjectDate(),
+  };
+
+  var compiledContent = templatesModule.render('descriptionfile', templateObj);
+
+  fs.writeFileSync(filePath, compiledContent);
+}
+
+
+module.exports = initMethods;
